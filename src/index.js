d3ce36a77f247709528e0fea5a6488ae53d0db9f
Maksim99745 <maksimmuhametzyanov11@gmail.com>
2023-08-15 18:54:52 +0300

    feat: solve reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function reverse (n) {
+    let i = 0;
+    let str = String(n);
+    let result = '';
+
+    while (i < str.length) {
+        if (n < 0) {
+        result = `${str[i]}${result}`;
+        } else if (n > 0)  {
+        result = `${str[i]}${result}`;
+        } else {
+          return 0;
+        }
+        i += 1;
+    }
+  let number = parseInt(result);
   
-}
+  let ads = Math.abs(number);
+  return ads;
+};
